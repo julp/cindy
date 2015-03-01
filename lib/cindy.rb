fc865c4dcde349618383f7d875f2d0d6726a0cf9
julp <e2c2be7ed0f2f336@gmail.com>
2015-03-01 21:11:40 +0100

    .rb not needed

--- lib/cindy.rb
+++ lib/cindy.rb
@@ -1,11 +1,11 @@
+require 'cindy/version'
+require 'cindy/cindy'
+require 'cindy/command'
+require 'cindy/environment'
+require 'cindy/variable'
+require 'cindy/template'
+require 'cindy/executor/ssh'
+require 'cindy/executor/local'
+
 module Cindy
 end
-
-require 'cindy/version.rb'
-require 'cindy/cindy.rb'
-require 'cindy/command.rb'
-require 'cindy/environment.rb'
-require 'cindy/variable.rb'
-require 'cindy/template.rb'
-require 'cindy/executor/ssh.rb'
-require 'cindy/executor/local.rb'
